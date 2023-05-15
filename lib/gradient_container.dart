8e345600fb236220ce93c0826ee57e5a7ae5e739
Askar Karasaev <askar.karasaev@gmail.com>
2023-05-15 17:34:00 +0900

    section-2 lesson-45

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -12,6 +12,10 @@ class GradientContainer extends StatelessWidget {
 
   final Color color1, color2;
 
+  void rollDice() {
+    //...
+  }
+
   @override
   Widget build(context) {
     return Container(
@@ -23,9 +27,17 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-2.png',
-          width: 200,
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('Roll Dice'),
+            )
+          ],
         ),
       ),
     );
